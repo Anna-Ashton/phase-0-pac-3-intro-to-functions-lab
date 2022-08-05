621eaddc3b80dd3a2a066a7b32e49df5adcc96b0
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-05 07:49:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+const string = "Hello"
+function shout(string) {
+    return 'Hello'.toUpperCase (); 'HELLO'
+}
+const whisperString = "Hello"
+function whisper(string) {
+    return 'hello'.toLowerCase (); 'hello'
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase()); "HELLO"
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase()); "hello"
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can\'t hear you!"
+    }
+    else if (string===string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    if (string=== "Let\'s have dinner together!") {
+        return "I would love to!"
+    }
+}
